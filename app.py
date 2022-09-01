37ea89bd94ea36a0d866975f0aa5e63f96c9ffbc
lozo6 <lorenzo.francisco@stonybrook.edu>
2022-09-01 01:37:06 -0400

    used information from class and class notes

--- app.py
+++ app.py
@@ -2,45 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# this is the home page that will display Hello World
+# created a page from templates folder taught in class
 @app.route('/')
 def hello():
-    return '<h1 style="text-align:center; font-family: Arial">Hello World</h1>'
+    return render_template('hello.html')
 
 # this page will show a heading and a button that has an alert
 @app.route('/login')
 def login():
-    return '''
-    <style>
-        button {font-family; Arial; font-size: 20px; padding: 7px; border-radius: 7px}
-        #container {text-align: center}
-    </style>
-
-    <h1 style="text-align:center; font-family: Arial">
-        This is the Login Page
-    </h1>
-
-    <div id='container'>
-        <button type='button' onclick='alert("This feature is not available at the moment")'>Click here to login</button>
-    </div>
-    '''
+    return render_template('login.html')
 
 # this page will show you a heading and a picture
 @app.route('/milkis')
 def milkis():
-    return '''
-    <style>
-        #container {text-align: center}
-    </style>
-
-    <h1 style="text-align:center; font-family: Arial">
-        This is a picture of milkis
-    </h1>
-
-    <div id='container'>
-        <img src='https://cdn.shopify.com/s/files/1/0338/0694/2253/products/LotteMilkisDrinkCanOriginalFlavor8.45oz_front_900x.jpg?v=1650574585' alt='milkis'>
-    </div>
-    '''
+    return render_template('milkis.html')
 
 # this is to test if the script can run on localhost
 if __name__ == '__main__':
