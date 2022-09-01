5350c20fc0f54a284c165077eab25bca866d7e63
lozo6 <lorenzo.francisco@stonybrook.edu>
2022-09-01 00:38:59 -0400

    added code

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # landing page
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    return '''<html>
+    <style>
+        h1 {text-align: center}
+    </style>
+
+    <h1>
+        Hello, World!
+    </h1>
+    </html>
+    '''
+
+@app.route('/login')
+def login():
+    return '''<html>
+    <style>
+        h1 {text-align: center}
+        button {font-size: 20px; padding: 7px; border-radius: 7px}
+        #container {text-align: center}
+    </style>
+
+    <h1>
+        This is the Login Page
+    </h1>
+
+    <div id='container'>
+        <button type='button' onclick='alert("This feature is not available at the moment")'>
+            Login
+        </button>
+    </div>
+    </html>
+    '''
+
+@app.route('/milkis')
+def milkis():
 
 # this is to test if the script can run on localhost
 if __name__ == '__main__':
