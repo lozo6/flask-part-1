bff728c5c1d006fb3cae8fb4df25088be6b9aff4
lozo6 <lorenzo.francisco@stonybrook.edu>
2022-09-01 01:17:56 -0400

    attempt to short code

--- app.py
+++ app.py
@@ -5,56 +5,41 @@ app = Flask(__name__)
 # this is the home page that will display Hello World
 @app.route('/')
 def hello():
-    return '''<html>
-    <style>
-        h1 {text-align: center; font-family: Arial}
-    </style>
-
-    <h1>
-        Hello, World!
-    </h1>
-    </html>
-    '''
+    return '<h1 style="text-align:center; font-family: Arial">Hello World</h1>'
 
 # this page will show a heading and a button that has an alert
 @app.route('/login')
 def login():
-    return '''<html>
+    return '''
     <style>
-        h1 {text-align: center; font-family: Arial}
         button {font-family; Arial; font-size: 20px; padding: 7px; border-radius: 7px}
         #container {text-align: center}
     </style>
 
-    <h1>
+    <h1 style="text-align:center; font-family: Arial">
         This is the Login Page
     </h1>
 
     <div id='container'>
-        <button type='button' onclick='alert("This feature is not available at the moment")'>
-            Login
-        </button>
+        <button type='button' onclick='alert("This feature is not available at the moment")'>Click here to login</button>
     </div>
-    </html>
     '''
 
 # this page will show you a heading and a picture
 @app.route('/milkis')
 def milkis():
-    return '''<html>
+    return '''
     <style>
-        h1 {text-align: center; font-family: Arial}
         #container {text-align: center}
     </style>
 
-    <h1>
+    <h1 style="text-align:center; font-family: Arial">
         This is a picture of milkis
     </h1>
 
     <div id='container'>
-        <img src='https://cdn.shopify.com/s/files/1/0338/0694/2253/products/LotteMilkisDrinkCanOriginalFlavor8.45oz_front_900x.jpg?v=1650574585'>
+        <img src='https://cdn.shopify.com/s/files/1/0338/0694/2253/products/LotteMilkisDrinkCanOriginalFlavor8.45oz_front_900x.jpg?v=1650574585' alt='milkis'>
     </div>
-    </html>
     '''
 
 # this is to test if the script can run on localhost
